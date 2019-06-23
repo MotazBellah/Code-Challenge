a33a4c9b900bd99eb1ad9fbfea0d3aa6ee890085
MoatazBellah <motazbellahhamdy@gmail.com>
2019-06-23 19:58:22 +0200

    check word spilling using Tries

---
+++ Algorithms/Is-word_Tries.py
@@ -0,0 +1,33 @@
+basic_trie = {
+    # a and add word
+    'a': {
+        'd': {
+            'd': {'word_end': True},
+            'word_end': False},
+        'word_end': True},
+    # hi word
+    'h': {
+        'i': {'word_end': True},
+        'word_end': False}}
+
+def is_word(word):
+    """
+    Look for the word in `basic_trie`
+    """
+    current_node = basic_trie
+
+    for char in word:
+        if char not in current_node:
+            return False
+
+        current_node = current_node[char]
+        
+
+    return current_node['word_end']
+
+test_words = ['ap', 'add']
+for word in test_words:
+    if is_word(word):
+        print('"{}" is a word.'.format(word))
+    else:
+        print('"{}" is not a word.'.format(word))
--- Algorithms/binary_search_recursive.py
+++ Algorithms/binary_search_recursive.py
@@ -1,7 +1,7 @@
 def binary_search(array, target):
 
     return binary_search_recursive(array, target, 0, len(array) - 1)
-    
+
 def binary_search_recursive(array, target, start_index, end_index):
     '''Write a function that implements the binary search algorithm using recursion
 
@@ -16,17 +16,15 @@ def binary_search_recursive(array, target, start_index, end_index):
     if start_index > end_index:
         return -1
 
-    #start_index = 0
-    #end_index = len(array) - 1
 
     mid_index = (start_index + end_index)//2
 
     mid_element = array[mid_index]
 
-    if target == mid_element:                       
+    if target == mid_element:
         return mid_index
-   
-    elif target < mid_element:                      
+
+    elif target < mid_element:
         return binary_search_recursive(array, target, start_index, mid_index - 1)                   # we will only search in the left half
-    else:                                        
+    else:
         return binary_search_recursive(array, target, mid_index + 1, end_index)
