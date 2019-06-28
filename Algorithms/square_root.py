37a1ae6b5e184d7d305eb8d3483366ca74626f8d
MoatazBellah <motazbellahhamdy@gmail.com>
2019-06-28 04:48:52 +0200

    Find square root

--- Algorithms/square_root.py
+++ Algorithms/square_root.py
@@ -7,7 +7,8 @@ def sqrt(number):
     Returns:
        int: Floored Square Root
     """
-    pass
+
+    return int((number ** 0.5))
 
 print ("Pass" if  (3 == sqrt(9)) else "Fail")
 print ("Pass" if  (0 == sqrt(0)) else "Fail")
