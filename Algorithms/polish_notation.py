a0fae9f98083d8a092eab172005ef51b8627a439
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-09-11 22:38:10 +0200

    Added unit test for polish-notation algorithm

--- Algorithms/polish_notation.py
+++ Algorithms/polish_notation.py
@@ -1,9 +1,22 @@
 import operator
+import unittest
 
-def x(expr):
+# Your job is to create a calculator which evaluates expressions in Reverse Polish notation.
+#
+# For example expression 5 1 2 + 4 * + 3 - (which is equivalent to 5 + ((1 + 2) * 4) - 3 in normal notation) should evaluate to 14.
+#
+# For your convenience, the input is formatted such that a space is provided between every token.
+#
+# Empty expression should evaluate to 0.
+#
+# Valid operations are +, -, *, /.
+#
+# You may assume that there won't be exceptional situations (like stack underflow or division by zero).
+
+def polish_notation(expr):
     OPERATORS = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv}
     stack = [0]
-    for token in expr.split(" ")[::-1]:
+    for token in expr.split(" "):
         if token in OPERATORS:
             op2, op1 = stack.pop(), stack.pop()
             print(op1, op2)
@@ -11,3 +24,18 @@ def x(expr):
         elif token:
             stack.append(float(token))
     return stack.pop()
+
+
+class TestPolishNotation(object):
+
+    def test_notation(self):
+        self.assertEqual(polish_notation('3.5'), 3.5)
+        self.assertEqual(polish_notation('1 3 +'), 4)
+        self.assertEqual(polish_notation('1 3 *'), 3)
+        self.assertEqual(polish_notation('1 3 -'), -2)
+        self.assertEqual(polish_notation('4 2 /'), 2)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
