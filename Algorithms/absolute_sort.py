ddc3804afc3cd9fe8dcf8d22b7a67afaf8194bb1
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-09-12 19:18:15 +0200

    solve absolute sort algorithm

--- Algorithms/absolute_sort.py
+++ Algorithms/absolute_sort.py
@@ -1,3 +1,7 @@
+# The array (a tuple) has various numbers.
+# You should sort it, but sort it by absolute value in ascending order.
+# For example, the sequence (-20, -5, 10, 15) will be sorted like so: (-5, 10, 15, -20)
+
 def abs_sort(n):
     return sorted(n, key=abs)
 
