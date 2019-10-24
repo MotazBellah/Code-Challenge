8ee54fbef29fab179a49da9e2f9e18ea324153d1
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-10-24 11:03:36 +0200

    Tested find uniq function using unittest

--- Algorithms/find_unique_number.py
+++ Algorithms/find_unique_number.py
@@ -1,6 +1,26 @@
+import unittest
+# There is an array with some numbers.
+# All numbers are equal except for one. Try to find it!
+
 def find_uniq(arr):
-    # your code here
+    # Get the set of the List
     set_list = set(arr)
+    # Loop through the set
+    # If the item in the set has shown once in the main array, return it
     for i in set_list:
         if arr.count(i) == 1:
             return i
+
+
+
+
+class TestFindUnique(unittest.TestCase):
+
+    def test_find_uniq(self):
+        self.assertEqual(find_uniq([ 1, 1, 1, 2, 1, 1 ]), 2)
+        self.assertEqual(find_uniq([ 0, 0, 0.55, 0, 0 ]), 0.55)
+        self.assertEqual(find_uniq([ 3, 10, 3, 3, 3 ]), 10)
+
+
+if __name__ == '__main__':
+    unittest.main()
