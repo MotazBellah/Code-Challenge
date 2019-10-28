38b0eedc0d2dcf9999e78a198d0b317c7e6cc0eb
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-10-28 22:40:53 +0200

    Added some comments

--- Algorithms/spin_words.py
+++ Algorithms/spin_words.py
@@ -1,5 +1,14 @@
+# Write a function that takes in a string of one or more words,
+# and returns the same string, but with all five or more letter words reversed .
+# Strings passed in will consist of only letters and spaces.
+# Spaces will be included only when more than one word is present.
+
 def spin_words(sentence):
+    # declare a list
     word_list = []
+    # loop through the string
+    # if the length of the word >= 5
+    # then reverse it
     for i in sentence.split():
         if len(i) >= 5:
             word_list.append(i[::-1])
