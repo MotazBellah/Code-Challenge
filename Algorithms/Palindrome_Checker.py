a685da06ecd6c0dcf79327f95e735c36d7e0b5a0
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-11-01 22:25:42 +0200

    Tested palindrom checker algorithm

--- Algorithms/Palindrome_Checker.py
+++ Algorithms/Palindrome_Checker.py
@@ -1,4 +1,27 @@
 import re
+import unittest
+
+# Write a function palindrome which returns true
+# if the string passed to it is a palindrome and false otherwise.
+# The function should ignore case and all non-alphabetical characters
+# (including spaces, punctuation marks and numbers).
+
 def palindrome(word):
+    # get only the string letters
     clear_word = re.sub(r'\W','',word).lower()
+    # return true if the word equal to the reversed word
     return clear_word == clear_word[::-1]
+
+
+
+class TestPalindrome(unittest.TestCase):
+
+    def test_palindrome(self):
+        self.assertEqual(palindrome(''), False)
+        self.assertEqual(palindrome("bob"), True)
+        self.assertEqual(palindrome("banana"), False)
+        self.assertEqual(palindrome("Madam In Eden, I’m Adam"), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
