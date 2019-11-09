0e8505b2f8b014b1eba27c47d9a82c30ca060295
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-11-09 22:07:22 +0200

    Tested an Algorithm to create all permutations of an input string and remove duplicates

--- Algorithms/permutations.py
+++ Algorithms/permutations.py
@@ -1,5 +1,22 @@
 import itertools
+import unittest
+# you have to create all permutations of an input string and remove duplicates,
+# if present. This means, you have to shuffle all letters from the input in all possible orders.
 
 def permutations(string):
+    # use itertools bilt-in lib
     y = ["".join(i) for i in list(itertools.permutations(string))]
+    # remove the dublicates
     return list(set(y))
+
+
+class TestPermutation(unittest.TestCase):
+
+    def test_permutations(self):
+        self.assertEqual(sorted(permutations('a')), ['a'])
+        self.assertEqual(sorted(permutations('ab')), ['ab', 'ba'])
+        self.assertEqual(sorted(permutations('aabb')), ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa'])
+
+
+if __name__ == '__main__':
+    unittest.main()
