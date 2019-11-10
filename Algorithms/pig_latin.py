19e60e90e5c2f8fb9e4ca43d289edb002f3dea5b
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-11-10 22:26:04 +0200

    Tested pig latin implementation

--- Algorithms/pig_latin.py
+++ Algorithms/pig_latin.py
@@ -1,3 +1,7 @@
+import unittest
+# Move the first letter of each word to the end of it,
+# then add "ay" to the end of the word.
+
 def pig_it(text):
     update_list = []
 
@@ -9,3 +13,15 @@ def pig_it(text):
              update_list.append(v)
 
     return " ".join(update_list)
+
+
+
+class TestPigLatin(unittest.TestCase):
+
+    def test_pig_it(self):
+        self.assertEqual(pig_it('Pig latin is cool'),'igPay atinlay siay oolcay')
+        self.assertEqual(pig_it('This is my string'),'hisTay siay ymay tringsay')
+
+
+if __name__ == '__main__':
+    unittest.main()
