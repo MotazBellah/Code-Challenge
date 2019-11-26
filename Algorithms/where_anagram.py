0131b20acd81b63b2b6e1f61634b79738af08a10
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-11-26 23:27:04 +0200

    Implemented an Algorithm to find all the anagrams of word from lis

--- Algorithms/where_anagram.py
+++ Algorithms/where_anagram.py
@@ -1,3 +1,7 @@
+# Write a function that will find all the anagrams of a word from a list.
+# You will be given two inputs a word and an array with words.
+# You should return an array of all the anagrams or an empty array if there are none
+
 import itertools
 def anagrams(word, words):
     output = []
