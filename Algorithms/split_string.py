78763748a2d64c8683604ff36890839486d69c6a
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-11-28 23:57:54 +0200

    Implemented an Algorithm to split the string

--- Algorithms/split_string.py
+++ Algorithms/split_string.py
@@ -1,9 +1,13 @@
+# Complete the solution so that it splits the string into pairs of two characters.
+# If the string contains an odd number of characters
+# then it should replace the missing second character of the final pair with an underscore ('_').
+
 def solution(s):
     lst = []
     if len(s) == 0:
         return lst
     else:
-        if len(s) % 2 != 0:
+        if len(s) % 2:
             s += "_"
         i, j = 0, 0
         while len(lst) < len(s) / 2:
