96561fcf76969eb28f70e38537e335396bcd1dbe
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-12-25 05:45:43 +0200

    Test the sum string algorithm and added some comments

--- Algorithms/sum_string.py
+++ Algorithms/sum_string.py
@@ -1,5 +1,24 @@
 import re
+import unittest
+
+# Given a random string consisting of
+# numbers, letters, symbols, you need to sum up the numbers in the string.
+
 def sum_from_string(string):
+    # Use regex to get all numbers in the string
     n = re.findall(r'[\d]+', string)
-    return sum([int(i) for i in list(n)])
-    
+    # sum all the numbers
+    return sum(int(i) for i in list(n))
+
+
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(sum_from_string("In 2015, I want to know how much does iPhone 6+ cost?"),2021)
+        self.assertEqual(sum_from_string("1+1=2"),4)
+        self.assertEqual(sum_from_string("e=mc^2"),2)
+        self.assertEqual(sum_from_string("Hi"),0)
+
+
+if __name__ == '__main__':
+    unittest.main()
