b32e1f8104c4eda6d7c7ae1edec4b9d0a277faf9
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-12-26 15:05:10 +0200

    Test algorithm to check if the pranthese are valid

--- Algorithms/vaild_parentheses.py
+++ Algorithms/vaild_parentheses.py
@@ -1,3 +1,5 @@
+import unittest
+
 def valid_parenth(s):
     if s[0] == ')' or s[-1] == '(' or len(s) % 2 != 0:
         return False
@@ -14,5 +16,18 @@ def valid_parenth(s):
             if (lastOpen, char) not in match:
                 return False
     return len(stack) == 0
+
+
+class TestParenth(unittest.TestCase):
+
+    def test_valid_parenth(self):
+        self.assertEqual(valid_parenth(")"),False)
+        self.assertEqual(valid_parenth("())"),False)
+        self.assertEqual(valid_parenth("()((((()())())))()"),True)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
 
 print (valid_parenth(")(()))"))
