310906cc493a06474c03afdf569686d15e567cfe
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-01-26 19:59:54 +0200

    Tested the algorithm that check if the word can be arranged to form palindrome

--- Algorithms/palindrome_rearranging.py
+++ Algorithms/palindrome_rearranging.py
@@ -1,3 +1,21 @@
+# Given a string, find out if its characters
+# can be rearranged to form a palindrome
+import unittest
+
 def palindromeRearranging(s):
     x = [i for i in set(s) if s.count(i) % 2]
     return len(x) <= 1
+
+
+class TestPalindromeRearranging(unittest.TestCase):
+
+    def test_palindromeRearranging(self):
+        self.assertEqual(palindromeRearranging("aabb"),True)
+        self.assertEqual(palindromeRearranging("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaabc"),False)
+        self.assertEqual(palindromeRearranging("abbcabb"),True)
+        self.assertEqual(palindromeRearranging("z"),True)
+        self.assertEqual(palindromeRearranging("abdhuierf"),False)
+
+
+if __name__ == '__main__':
+    unittest.main()
