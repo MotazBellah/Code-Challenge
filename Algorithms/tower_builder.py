1a3440a9c03308591c52473f6f42de59b63c1244
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-02-02 03:00:06 +0200

    Solve the tower builder challenge

--- Algorithms/tower_builder.py
+++ Algorithms/tower_builder.py
@@ -1,8 +1,19 @@
+# Build Tower by the following given argument:
+# number of floors (integer and always greater than 0).
+#
+# Tower block is represented as *
+# for example, a tower of 3 floors looks like below
+# [
+#   '  *  ',
+#   ' *** ',
+#   '*****'
+# ]
+
 def tower_builder(n):
     tower = []
     count = 0
     for i in range(n):
-        line = ' ' *(n-i-1)+ '*' + (count* '*') + ' ' *(n-i-1)
+        floor = ' ' *(n-i-1)+ '*' + (count* '*') + ' ' *(n-i-1)
         count += 2
-        tower.append(line)
+        tower.append(floor)
     return tower
