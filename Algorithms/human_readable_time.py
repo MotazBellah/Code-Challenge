f9873d41c39f1ef68a25a3c38892dbb3ebe200cd
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-02-12 15:32:45 +0200

    solve human-readable-time problem to convert the secondes to format HH:MM:SS

--- Algorithms/human_readable_time.py
+++ Algorithms/human_readable_time.py
@@ -1,2 +1,12 @@
+# Write a function, which takes a non-negative integer (seconds) as input
+# and returns the time in a human-readable format (HH:MM:SS)
+#
+# HH = hours, padded to 2 digits, range: 00 - 99
+# MM = minutes, padded to 2 digits, range: 00 - 59
+# SS = seconds, padded to 2 digits, range: 00 - 59
+# The maximum time never exceeds 359999 (99:59:59)
+#
+# You can find some examples in the test fixtures.
+
 def make_readable(s):
     return '{:02}:{:02}:{:02}'.format(s / 3600, s / 60 % 60, s % 60)
