d150903380ee993d7964eb59dc2ae96478f17c85
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-03-08 23:34:25 +0200

    Convert hex to dec

--- Algorithms/hex.py
+++ Algorithms/hex.py
@@ -1,3 +1,8 @@
+# Write a program that will convert a hexadecimal number
+# represented as a string (e.g. "10af8c"), to its decimal equivalent.
+#
+# The program should return -1 for invalid hexadecimal strings.
+
 def hexa_first_principles(hexadecimal):
     try:
         return int(hexadecimal, 16)
