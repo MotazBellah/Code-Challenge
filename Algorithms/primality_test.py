7ebb5c5f50751e83f0adc72d03d1547fca2307f7
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-03-14 21:48:43 +0200

    Create a function to return true if the input is prime

--- Algorithms/primality_test.py
+++ Algorithms/primality_test.py
@@ -1,2 +1,6 @@
+# Create the function prime that returns true when the given input is prime,
+# and false when it is composite.
+# All inputs will be positive integers.
+
 def prime(n):
-    return all(n%i for i in range(2,int(n**.5)+1)) 
+    return all(n%i for i in range(2,int(n**.5)+1))
