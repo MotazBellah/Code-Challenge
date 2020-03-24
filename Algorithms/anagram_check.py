3852b1f377e036b256392e643729c538a86e9fcc
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-03-24 16:17:15 +0200

    Solve anagram checker using dict

--- Algorithms/anagram_check.py
+++ Algorithms/anagram_check.py
@@ -1,3 +1,6 @@
+# Given two strings, check to see if they are anagrams
+# An anagram is a word or phrase formed by rearranging the letters of a different word or phrase
+
 def anagram_check(str1, str2):
     # ignore space and capitalizatio
     str1 = str1.replace(' ', '').lower()
@@ -5,4 +8,23 @@ def anagram_check(str1, str2):
     return sorted(str1) == sorted(str2)
 
 
-print(anagram_check('public relations', 'crap built on lies'))
+def anagram_check_dict(str1, str2):
+    # ignore space and capitalizatio
+    str1 = str1.replace(' ', '').lower()
+    str2 = str2.replace(' ', '').lower()
+
+    # edge case check
+    if len(str1) != len(str2):
+        return False
+
+    count1 = {}
+    count2 = {}
+    for i in range(len(str1)):
+        count1[str1[i]] = str1.count(str1[i])
+        count2[str2[i]] = str2.count(str2[i])
+
+    return count1 == count2
+
+
+print(anagram_check_dict('public relations', 'crap built on lies'))
+print(anagram_check_dict('clint eastwood', 'old west action'))
