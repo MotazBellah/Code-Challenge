3f7dc17faf076246ed155b9b279ba9e5e5eb809f
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-03-28 17:49:24 +0200

    implement stacks, queue and dequeue

---
+++ Data-Structure/implement_dequeue.py
@@ -0,0 +1,34 @@
+# Deque() creates a new deque that is empty. It needs no parameters and returns an empty deque.
+# addFront(item) adds a new item to the front of the deque. It needs the item and returns nothing.
+# addRear(item) adds a new item to the rear of the deque. It needs the item and returns nothing.
+# removeFront() removes the front item from the deque. It needs no parameters and returns the item. The deque is modified.
+# removeRear() removes the rear item from the deque. It needs no parameters and returns the item. The deque is modified.
+# isEmpty() tests to see whether the deque is empty. It needs no parameters and returns a boolean value.
+# size() returns the number of items in the deque. It needs no parameters and returns an integer.
+
+class Dequeue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty():
+        return self.items == []
+
+    def addFront(self, item):
+        self.items.append(item)
+
+    def addRear(self, item):
+        self.items.insert(0, item)
+
+    def removeFront(self):
+        return self.items.pop()
+
+    def removeRear(self):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
+
+d = Dequeue()
+d.addFront('hello')
+d.addRear('World')
+print(d.removeFront() + ' ' + d.removeRear())
