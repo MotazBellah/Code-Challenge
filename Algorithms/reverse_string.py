fcbdc0e1a440600903d6a7ca04dfc8819fc01e5f
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-03-30 01:19:51 +0200

    Resolve the reverse_string using recursion and without slice or iteration

--- Algorithms/reverse_string.py
+++ Algorithms/reverse_string.py
@@ -1,3 +1,18 @@
+# Reverse string with Recursion
+# Do not slice (e.g. string[::-1]) or use iteration, there must be a recursive call for the function.
+def reverse(s, output=''):
+    if s and len(s) != len(output):
+        i = len(output) + 1
+        output += s[-i]
+        return reverse(s, output)
+    else:
+        return output
+
+
+print(reverse('hello world'))
+print(reverse('abcdef ghiA'))
+
+
 def string_reverser(our_string):
 
     """
@@ -8,5 +23,5 @@ def string_reverser(our_string):
     Returns:
        string: The reversed string
     """
-    
+
     return our_string[::-1]
