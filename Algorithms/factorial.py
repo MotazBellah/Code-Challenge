7761054db41a86f694d24e3f97c97ce016d756dc
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-03-30 00:50:33 +0200

    Resolve the factorial using the memoization

--- Algorithms/factorial.py
+++ Algorithms/factorial.py
@@ -7,3 +7,21 @@ def factorial(n):
         return n * factorial(n-1)
 
 print(factorial(5))
+
+
+
+# Memoization effectively refers to remembering ("memoization" -> "memorandum" -> to be remembered)
+# results of method calls based on the method inputs and
+# then returning the remembered result rather than computing the result again
+# Create cache for known results
+factorial_memo = {}
+
+def factorial_memoization(k):
+
+    if k < 2:
+        return 1
+
+    if not k in factorial_memo:
+        factorial_memo[k] = k * factorial(k-1)
+
+    return factorial_memo[k]
