dfe7f1e86b28294a20e2ee544319d01eacf6487d
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-03-30 19:39:41 +0200

    Solve fibonacci using recursion and dynamic programming

--- Algorithms/fibonacci.py
+++ Algorithms/fibonacci.py
@@ -6,4 +6,20 @@ def fib_rec(n):
     else:
         return fib_rec(n-1) + fib_rec(n-2)
 
-print(fib_rec(10))
+# print(fib_rec(10))
+
+
+memo = {}
+
+def fib_dyn(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        if not n in memo:
+            memo[n] = fib_rec(n-1) + fib_rec(n-2)
+
+        return memo[n]
+
+print(fib_dyn(11))
