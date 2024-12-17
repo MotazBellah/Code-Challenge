399f7a2ac7eda10b52b1ec92af0e79b5fe65a5fe
MotazBellah <moataz.ghobashi@gmail.com>
2024-12-17 23:25:48 +0100

    day 7

---
+++ AOC/day_7/day_7.py
@@ -0,0 +1,54 @@
+class BridgeRepair:
+    def read_input(self):
+        with open("input.txt", 'r') as f:
+            for line in f:
+                x = [i for i in line.strip().split(":")]
+                lhs = int(x[0])
+                rhs = [int(i) for i in x[1].split()]
+                yield (lhs, rhs)
+
+    def part1(self):
+        total = 0
+        for lhs, rhs in self.read_input():
+            left_numbers = [rhs.pop(0)]
+            while rhs:
+                curr = rhs.pop(0)
+                check = []
+                print(curr)
+                for n in left_numbers:
+                    check.append( n + curr)
+                    check.append( n * curr)
+                left_numbers = check
+
+            if lhs in left_numbers:
+                total += lhs
+        return total
+
+    def part2(self):
+        total = 0
+        for lhs, rhs in self.read_input():
+            left_numbers = [rhs.pop(0)]
+            while rhs:
+                curr = rhs.pop(0)
+                check = []
+                print(curr)
+                for n in left_numbers:
+                    check.append( n + curr)
+                    check.append( n * curr)
+                left_numbers = check
+
+            if lhs in left_numbers:
+                total += lhs
+        return total
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    r = BridgeRepair()
+    part1 = r.part1()
+
+    print(part1)
